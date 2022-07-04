5b8aaa019e235d076b00fa3912115fcf5a74b19d
Lol7a <lola.lapic@gmail.com>
2022-07-04 22:53:12 +0200

    Forms

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,13 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe(
@@ -28,14 +30,20 @@ export class RecipeService {
       'This easy vegan pasta is basically everything I want to eat for dinner any night of the week! An easy creamy tomato sauce (just cashews+marinara!) coats a 50/50 mix of fresh veggies and floppy rigatoni noodles.',
       'https://cdn.loveandlemons.com/wp-content/uploads/2019/12/IMG_17194-580x857.jpg',
       [
-        new Ingredient('White Beans', 1),
-        new Ingredient('Lemon Juice', 3),
-        new Ingredient('Olive Oil', 4),
-        new Ingredient('Nutritional Yeast', 1),
-        new Ingredient('Yellow Onion', 1),
-        new Ingredient('Broccoli', 5),
-        new Ingredient('Pine Nuts', 5),
-        new Ingredient('Garlic Clove', 1),
+        new Ingredient('Extra-virgin Olive Oil', 1),
+        new Ingredient('Finely Minced Shallot', 3),
+        new Ingredient('Large Garlic Clove', 1),
+        new Ingredient('Sea Salt', 1),
+        new Ingredient('Black Pepper', 1),
+        new Ingredient('Chopped Tomatoes', 1),
+        new Ingredient('Balsamic Vinegar', 1),
+        new Ingredient('Dried Oregano', 1),
+        new Ingredient('Red Pepper Flakes', 1),
+        new Ingredient('Rigatoni', 10),
+        new Ingredient('Zucchini', 2),
+        new Ingredient('Thyme Leaves', 2),
+        new Ingredient('Cherry Tomatoes', 2),
+        new Ingredient('Spinach', 6),
       ]
     ),
     new Recipe(
@@ -43,14 +51,19 @@ export class RecipeService {
       'Who says you need cheese to make a crave-worthy pizza? This easy vegan pizza may be my all-time favorite! I top chewy homemade pizza dough with a lemony cashew cream sauce, a jumble of savory, spicy, crisp, and juicy veggies, and big handfuls of fresh basil',
       'https://cdn.loveandlemons.com/wp-content/uploads/2019/12/IMG_14058-580x826.jpg',
       [
-        new Ingredient('White Beans', 1),
-        new Ingredient('Lemon Juice', 3),
-        new Ingredient('Olive Oil', 4),
-        new Ingredient('Nutritional Yeast', 1),
-        new Ingredient('Yellow Onion', 1),
-        new Ingredient('Broccoli', 5),
-        new Ingredient('Pine Nuts', 5),
-        new Ingredient('Garlic Clove', 1),
+        new Ingredient('Broccoli', 1),
+        new Ingredient('Cherry Tomatoes', 3),
+        new Ingredient('Kernels', 1),
+        new Ingredient('Red Onio', 1),
+        new Ingredient('Jalapeno', 0.5),
+        new Ingredient('Sun-dried Tomatoes', 4),
+        new Ingredient('Extra-virgin Olive Oil', 1),
+        new Ingredient('Pizza Dough', 1),
+        new Ingredient('Basil', 1),
+        new Ingredient('Thyme Leaves', 2),
+        new Ingredient('Red Pepper Flakes', 1),
+        new Ingredient('Sea Salt', 1),
+        new Ingredient('Black Pepper', 1),
       ]
     ),
     new Recipe(
@@ -97,5 +110,20 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,14 +1,20 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable()
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
   ];
+
+  getIngredient(index: number) {
+    return this.ingredients[index];
+  }
 
   getIngredients() {
     return this.ingredients.slice();
@@ -16,11 +22,21 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
