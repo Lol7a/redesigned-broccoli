03b560d8542da50b5eec0acad31e1a9d1d0ee7cf
Lol7a <lola.lapic@gmail.com>
2022-07-09 13:38:02 +0200

    Authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-// import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
-// import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
@@ -16,6 +16,7 @@ const appRoutes: Routes = [
   {
     path: 'recipes',
     component: RecipesComponent,
+    canActivate: [AuthGuard],
     children: [
       { path: '', component: RecipeStartComponent },
       { path: 'new', component: RecipeEditComponent },
@@ -36,6 +37,7 @@ const appRoutes: Routes = [
     component: ShoppingListComponent,
     children: [{ path: 'edit', component: ShoppingEditComponent }],
   },
+  { path: 'auth', component: AuthComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
